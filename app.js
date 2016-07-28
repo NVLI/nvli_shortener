706860186f4c07379d99d8365b2ef3531c3def1f
piyuesh23 <piyuesh23@qed42.com>
2016-07-28 16:18:49 +0530

    Auth for mongo

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var base58 = require('./base58.js');
 // grab the url model
 var Url = require('./models/url');
 
-mongoose.connect('mongodb://' + config.db.user + ':' + config.db.password + '@' + config.db.host + '/' + config.db.name);
+mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name, config.db.options);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- config.js
+++ config.js
@@ -5,7 +5,9 @@ config.webhost = 'http://stage-nvli.iitb.ac.in/';
 
 config.db.host = 'localhost';
 config.db.name = 'url_shortener';
-config.db.user = 'admin';
-config.db.password = 'ghg~GJpK3f';
+config.db.options = {
+  'user': 'root',
+  'password': 'ghg~GJpK3f'
+};
 
 module.exports = config;
