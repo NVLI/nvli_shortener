01af50cec44bc1f1c932f4b539d29b10e931433e
piyuesh23 <piyuesh23@qed42.com>
2016-07-28 16:01:17 +0530

    Securing mongo

--- config.js
+++ config.js
@@ -5,5 +5,7 @@ config.webhost = 'http://stage-nvli.iitb.ac.in/';
 
 config.db.host = 'localhost';
 config.db.name = 'url_shortener';
+config.db.user = 'root';
+config.db.password = 'NrWnfxCnh77';
 
 module.exports = config;
